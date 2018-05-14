b2e8ed44fe8d4a81afa56a3735ff31d85c3cdde2
danieltseng <danieltseng8612@gmail.com>
2018-05-15 01:39:00 +0800

    jnvytgyiholkmi

---
+++ js/script.js
@@ -0,0 +1,28 @@
+/**
+ * Frame-by-frame video animation with ScrollMagic and GSAP
+ *
+ * Note that your web server must support byte ranges (most do).
+ * Otherwise currentTime will always be 0 in Chrome.
+ * See here: http://stackoverflow.com/a/5421205/950704
+ * and here: https://bugs.chromium.org/p/chromium/issues/detail?id=121765
+ */
+window.onload = function(){
+    // your code
+
+    // select video element
+  var vid = document.getElementById('v0');
+  //var vid = $('#v0')[0]; // jquery option
+
+  // pause video on load
+  vid.pause();
+
+  // pause video on document scroll (stops autoplay once scroll started)
+  window.onscroll = function(){
+      vid.pause();
+  };
+
+  // refresh video frames on interval for smoother playback
+  setInterval(function(){
+      vid.currentTime = window.pageYOffset/200;
+  }, 200);
+  };
